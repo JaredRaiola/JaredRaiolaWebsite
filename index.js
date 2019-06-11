da3b64e9db418e93cd109e9f169ecd990950037d
JaredRaiola <jraiola3@gatech.edu>
2019-06-11 14:56:24 -0400

    Test of key withdrawal

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ function displayPeople(depName) {
     fbRef.once("value", function(person) {
         person.forEach(function (inPerson){
             newID++;
-            peopleKeys.append(inPerson.key);
+            peopleKeys.push(inPerson.key);
             $("#entries").append(rowForPerson(inPerson.val()));
         });
     });
