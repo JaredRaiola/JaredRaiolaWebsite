88507ce3404f1e666c40df4ab773842c3f8a4719
jaredraiola <jaredraiola@gmail.com>
2026-05-03 20:14:34 -0400

    feat: scaffold Vite + React + TypeScript

---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import '98.css';
+import './styles/reset.css';
+import App from './App';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
